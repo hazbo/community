a4eb7ac325b8f4734962cdbfc020434b79c167d7
Harry Lawrence <harry.lawrence@groundsix.com>
2013-01-22 20:33:21 +0000

    Modified version

--- Outglow/Component/Community/Community.php
+++ Outglow/Component/Community/Community.php
@@ -11,7 +11,7 @@
  * @author Harry Lawrence
  * @copyright Outglow Components 2012
  * @package Community
- * @version 1.0
+ * @version 1.1 Stable
  * @license The MIT License (MIT)
 */
 
@@ -129,6 +129,7 @@ class Community implements CommunityInterface
 			$this->checkForNewInstance($newInstance, $key);
 			return $this->container[$key] = $return;
 		}
+		return false;
 	}
 
 	/**
