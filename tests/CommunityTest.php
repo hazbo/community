315d7d504da152db6cfc03a29291f2d6910dcef0
Harry Lawrence <harry.lawrence@groundsix.com>
2013-01-22 20:30:29 +0000

    Added unit tests

---
+++ tests/CommunityTest.php
@@ -0,0 +1,63 @@
+<?php
+
+require_once('../Outglow/Component/Community/CommunityInterface.php');
+require_once('../Outglow/Component/Community/Community.php');
+
+use Outglow\Component\Community\Community;
+
+class CommunityTest extends PHPUnit_Framework_TestCase
+{
+	private $community;
+
+	/**
+	 * - setUp
+	 * CREATES A NEW COMMUNITY OBJECT
+	 */
+	public function setUp()
+	{
+		$this->community = new Community();
+	}
+
+	/**
+	 * - testSet
+	 * TESTS PUBLIC SET METHOD
+	 */
+	public function testSet()
+	{
+		$result = $this->community->set('String', function() {
+			return 'string';
+		});
+		$expected = true;
+		$this->assertTrue($result == $expected);
+	}
+
+	/**
+	 * - testGet
+	 * TESTS PUBLIC GET METHOD
+	 */
+	public function testGet()
+	{
+		$this->community->set('String', function() {
+			return 'string';
+		});
+		$result   = $this->community->get('String');
+		$expected = 'string';
+		$this->assertTrue($result == $expected);
+	}
+
+	/**
+	 * - testRemove
+	 * TESTS PUBLIC REMOVE METHOD
+	 */
+	public function testRemove()
+	{
+		$this->community->set('String', function() {
+			return 'string';
+		});
+		$result   = $this->community->remove('String');
+		$expected = true;
+		$this->assertTrue($result == $expected);
+	}
+}
+
+?>
