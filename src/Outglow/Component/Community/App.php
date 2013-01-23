2556efec3792f36768454c5eb014a526dae63964
Harry Lawrence <harry.lawrence@groundsix.com>
2013-01-23 13:32:04 +0000

    Added app class

---
+++ src/Outglow/Component/Community/App.php
@@ -0,0 +1,25 @@
+<?php namespace Outglow\Component\Community;
+
+/**
+ * A SIMPLE / SMALL DEPENDENCY INJECTION
+ * CONTAINER FOR PHP 5.3.0 + 5.4.0
+ * 
+ * FEEL FREE TO USE / MODIFY ANY OF THIS
+ * CODE FOR YOUR OWN PROJECTS
+ * OPEN SOURCE / COMMERCIAL
+ *
+ * @author Harry Lawrence
+ * @copyright Outglow Components 2013
+ * @package Community
+ * @version 1.1 Stable
+ * @license The MIT License (MIT)
+*/
+
+class App
+{
+	public function __construct()
+	{
+		
+	}
+}
+?>
