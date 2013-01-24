345aac97867faeb585e2ae9d4f6d552291fa53b0
Harry Lawrence <harry.lawrence@groundsix.com>
2013-01-24 14:22:12 +0000

    Added docs for router processor.

--- src/Outglow/Component/Community/Router/Processor.php
+++ src/Outglow/Component/Community/Router/Processor.php
@@ -1,7 +1,29 @@
 <?php namespace Outglow\Component\Community;
 
+/**
+ * A SIMPLE / SMALL DEPENDENCY INJECTION
+ * CONTAINER FOR PHP 5.3.0 + 5.4.0
+ * 
+ * FEEL FREE TO USE / MODIFY ANY OF THIS
+ * CODE FOR YOUR OWN PROJECTS
+ * OPEN SOURCE / COMMERCIAL
+ *
+ * @author Harry Lawrence
+ * @copyright Outglow Components 2013
+ * @package Community
+ * @version 1.2 Stable
+ * @license The MIT License (MIT)
+*/
+
 class Router_Processor
 {
+	/**
+	 * - overrideEmptyRouteToSingleSlash
+	 * IF ROUTE IS AN EMPTY STRING IT SETS IT
+	 * TO A FORWARD SLASH INSTEAD
+	 * @param String
+	 * @return String
+	 */
 	public function overrideEmptyRouteToSingleSlash($route)
 	{
 		return ($route == '') ? '/' : $route;
