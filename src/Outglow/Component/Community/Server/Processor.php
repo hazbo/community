70ae286045c7824b64b14df9c54cd57ab6dfbe5a
Harry Lawrence <harry.lawrence@groundsix.com>
2013-01-24 14:20:52 +0000

    Added docs for the server processor

--- src/Outglow/Component/Community/Server/Processor.php
+++ src/Outglow/Component/Community/Server/Processor.php
@@ -1,7 +1,29 @@
 <?php namespace Outglow\Component\Community;
 
+/**
+ * A SIMPLE / SMALL DEPENDENCY INJECTION
+ * CONTAINER FOR PHP 5.3.0 + 5.4.0
+ * 
+ * FEEL FREE TO USE / MODIFY ANY OF THIS
+ * CODE FOR YOUR OWN PROJECTS
+ * OPEN SOURCE / COMMERCIAL
+ *
+ * @author Harry Lawrence
+ * @copyright Outglow Components 2013
+ * @package Community
+ * @version 1.2 Stable
+ * @license The MIT License (MIT)
+*/
+
 class Server_Processor
 {
+	/**
+	 * STORES STRING VALUES
+	 * FROM SERVER SUPERGLOBALS
+	 * @var String
+	 * @var String
+	 * @var String
+	 */
 	private $httpHost;
 	private $requestUri;
 	private $requestString;
@@ -11,16 +33,34 @@ class Server_Processor
 		return $this->httpHost = $_SERVER['HTTP_HOST'];
 	}
 
+	/**
+	 * - setRequestUri
+	 * SETS THE REQUEST URI
+	 * PROPERTY
+	 * @return Bool
+	 */
 	private function setRequestUri()
 	{
 		return $this->requestUri = $_SERVER['REQUEST_URI'];
 	}
 
+	/**
+	 * - concatFullRequestString
+	 * CONCATS THE FULL REQUEST
+	 * STRING READY TO BE USED
+	 * @return Bool
+	 */
 	private function concatFullRequestString()
 	{
 		return $this->requestString = 'http://' . $this->httpHost . $this->requestUri;
 	}
 
+	/**
+	 * - constructor
+	 * SETS ALL THE INITIAL CLASS
+	 * PROPERTIES
+	 * @return null
+	 */
 	public function __construct()
 	{
 		$this->setHttpHost();
@@ -28,11 +68,24 @@ class Server_Processor
 		$this->concatFullRequestString();
 	}
 
+	/**
+	 * - getRequestString
+	 * GETS THE FULLY FORMED REQUEST
+	 * STRING
+	 * @return String
+	 */
 	public function getRequestString()
 	{
 		return $this->requestString;
 	}
 
+	/**
+	 * - processRequestStringForRoute
+	 * PROCESSES THE REQUEST STRING TO
+	 * HELP FIND OUT WHAT THE ROUTE
+	 * ACTUALLY IS
+	 * @return String
+	 */
 	public function processRequestStringForRoute($baseUrl)
 	{
 		return end(explode($baseUrl, $this->requestString));
